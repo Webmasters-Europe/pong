9b301af1362e1827761e01fc6b78eba3641daf87
Cosnavel <niclasdevelop@gmail.com>
2020-12-05 12:58:30 +0100

    Code Meetup 05.12
    Ball moves and bounces from the left and right wall. When hitting the computer or player bat, the ball also reflects. When the ball hits the top or bottom border, he reset.

--- src/index.js
+++ src/index.js
@@ -6,16 +6,24 @@ const ctx = canvas.getContext('2d');
 const width = 500;
 const height = 700;
 
-const canvasPosition = window.screen.width / 2 - width / 2;
+const canvasPosition = window.innerWidth / 2 - width / 2;
 
 let widthBatUser = 225;
 let widthBatComputer = 225;
 
 const batHeight = 10;
 const batWidth = 50;
+const batDiff = 25;
 
 let ballX = 250;
 let ballY = 350;
+
+let playerMoved = false;
+let batContact = false;
+
+let speedY;
+let speedX;
+let trajectoryX;
 
 let scorePlayer = 0;
 let scoreComputer = 0;
@@ -32,7 +40,7 @@ function renderCanvas() {
     ctx.fillRect(0, 0, width, height);
 
     ctx.fillStyle = 'white';
-    ctx.fillRect(widthBatUser, height - 25, batWidth, batHeight);
+    ctx.fillRect(widthBatUser, height - batDiff, batWidth, batHeight);
     ctx.fillRect(widthBatComputer, 15, batWidth, batHeight);
 
     ctx.beginPath();
@@ -51,22 +59,84 @@ function renderCanvas() {
     ctx.fillText(scoreComputer, 20, canvas.height / 2 - 30);
 }
 
+function ballReset() {
+    ballX = width / 2;
+    ballY = height / 2;
+    speedY = -3;
+    batContact = false;
+}
+
+function ballMove() {
+    ballY += -speedY;
+    if (playerMoved && batContact) {
+        ballX += speedX;
+    }
+}
+function ballBoundries() {
+    //left wall
+    if (ballX < 0) {
+        speedX = -speedX;
+    }
+
+    //right wall
+    if (ballX > width) {
+        speedX = -speedX;
+    }
+
+    //player bat
+    if (ballY > height - batDiff) {
+        if (ballX > widthBatUser && ballX < widthBatUser + batWidth) {
+            batContact = true;
+            if (playerMoved) {
+                speedY -= 1;
+                if (speedY < -10) {
+                    speedY = -10;
+                }
+            }
+            speedY = -speedY;
+            trajectoryX = ballX - (widthBatUser + batDiff);
+            speedX = trajectoryX * 0.3;
+        } else if (ballY > height) {
+            ballReset();
+        }
+    }
+
+    //computer bat
+    if (ballY < batDiff) {
+        if (ballX > widthBatComputer && ballX < widthBatComputer + batWidth) {
+            batContact = true;
+            if (playerMoved) {
+                speedY += 1;
+                if (speedY > 10) {
+                    speedY = 10;
+                }
+            }
+            speedY = -speedY;
+            trajectoryX = ballX - (widthBatComputer + batDiff);
+            speedX = trajectoryX * 0.3;
+        } else if (ballY < 0) {
+            ballReset();
+        }
+    }
+}
+
 function animate() {
     renderCanvas();
+    ballMove();
+    ballBoundries();
+    window.requestAnimationFrame(animate);
 }
 
 const startGame = () => {
     scorePlayer = 0;
     scoreComputer = 0;
-
+    ballReset();
     createCanvas();
-
-    setInterval(animate, 1000 / 60);
+    animate();
 
     canvas.addEventListener('mousemove', event => {
+        playerMoved = true;
         widthBatUser = event.clientX - canvasPosition - batWidth / 2;
-        console.log(event);
-        console.log(event.clientX);
         canvas.style.cursor = 'none';
     });
 };
